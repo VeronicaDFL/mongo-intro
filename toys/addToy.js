6eea1f16da9dec7a05e31c59dfb26fe776b9c2f6
Veronica De Felice <verodefelic@gmail.com>
2023-03-30 17:34:33 -0400

    Mongo Homerwork Done

--- toys/addToy.js
+++ toys/addToy.js
@@ -6,14 +6,47 @@ const addToy =  async () => {
 // get collection?
 
 const myToy = {
-    name: 'Optimus Prime',
-    type: 'Trasformer',
+    name: 'Truck',
+    type: 'toys on wheels',
     cool: true,
-    price: 40,
-    description: 'Leader Autobot'
+    price: 15,
+    description: 'Working Truck'
+
 }
 
 const addedToy = await toysCollection.insertOne(myToy)
 console.log()
 }
-addToy()
+// addToy()
+
+const editToy = async () => {
+
+const updateToy = await toysCollection.findOneAndUpdate(
+
+   { name: 'Mini Ferrari'},
+   {$set: {description: 'car'}}
+)
+   console.log(updateToy)
+}
+
+editToy()
+
+const getAllTheToys = async () => {
+
+try { 
+    
+    await client.connect()
+    const allToys =  await toysCollection.find().toArray()
+    console.log(allToys)
+
+    
+} catch (error) {
+    console.log(error)
+    
+}finally{
+    await client.close()
+
+}
+
+}
+// getAllTheToys()
